6867dd553be131173f86bebe8d03a2263b670457
flyinacres <ron-github@rfgf.com>
2023-01-29 15:03:38 -0800

    Setting up the notes

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,14 @@
 <template>
 	<div class="notes">
-		<h1>Notes</h1>
+		<div class="card mb-4" >
+			<div class="card-content">
+				Labore adipisicing et Lorem Lorem amet eu laboris. Aute fugiat enim quis ullamco id nisi. Excepteur aute qui sint cillum laboris reprehenderit nulla sint commodo velit cillum. Et ullamco consectetur veniam eu cillum aliquip dolor. Incididunt do occaecat culpa id ullamco ipsum aute duis excepteur adipisicing.
+			</div>
+			<footer class="card-footer">
+				<a href="#" class="card-footer-item">Save</a>
+				<a href="#" class="card-footer-item">Edit</a>
+				<a href="#" class="card-footer-item">Delete</a>
+			</footer>
+		</div>
 	</div>
 </template>
