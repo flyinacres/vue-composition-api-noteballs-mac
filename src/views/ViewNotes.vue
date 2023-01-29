69115549c242547b0543e2dde0a85ad6c6bfe0f4
flyinacres <ron-github@rfgf.com>
2023-01-29 15:21:54 -0800

    Notes page

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,5 +1,21 @@
 <template>
 	<div class="notes">
+
+		<div class="card has-background-success-dark p=4 mb-5">
+			<div class="field">
+				<div class="control">
+					<textarea class="textarea" placeholder="Add a new note"></textarea>
+				</div>
+			</div>
+
+			<div class="field is-grouped is-grouped-right">
+				<div class="control">
+					<button class="button is-link has-background-success">Add New Note</button>
+				</div>
+			</div>
+		</div>
+
+
 		<div class="card mb-4" >
 			<div class="card-content">
 				Labore adipisicing et Lorem Lorem amet eu laboris. Aute fugiat enim quis ullamco id nisi. Excepteur aute qui sint cillum laboris reprehenderit nulla sint commodo velit cillum. Et ullamco consectetur veniam eu cillum aliquip dolor. Incididunt do occaecat culpa id ullamco ipsum aute duis excepteur adipisicing.
