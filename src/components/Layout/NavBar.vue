0521ee65c8628baaa76858783ed2c6e82d266262
flyinacres <ron-github@rfgf.com>
2023-01-28 23:44:21 -0800

    More navbar look--again, none of it works in Bulma

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -1,20 +1,31 @@
 <template>
-	<nav class="navbar" role="navigation" aria-label="main navigation">
-		<div class="navbar-brand">
-			<div class="navbar-item is-size-4 is-family-monospace" >
-				Noteballs
-			</div>
+	<nav class="navbar is-danger" role="navigation" aria-label="main navigation">
+		<div class="container is-max-desktop">
+			<div class="navbar-brand">
+				<div class="navbar-item is-size-4 is-family-monospace" >
+					Noteballs
+				</div>
 
-			<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-			<span aria-hidden="true"></span>
-			<span aria-hidden="true"></span>
-			<span aria-hidden="true"></span>
-			</a>
+				<a role="button" 
+					@click.prevent="showMobileNav = !showMobileNav"
+					class="navbar-burger" 
+					:class="{ 'is-active' : showMobileNav }"
+					aria-label="menu" 
+					aria-expanded="false" 
+					data-target="navbarBasicExample">
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				<span aria-hidden="true"></span>
+				</a>
+			</div>
 		</div>
 
-		<div id="navbarBasicExample" class="navbar-menu">
 
-			<div class="navbar-end">
+		<div id="navbarBasicExample" 
+			class="navbar-menu"
+			:class="{ 'is-active' : showMobileNav }">
+
+			<div class="navbar-end is-danger">
 				<RouterLink to="/" class="navbar-item:" active-class="is-active">
 					Notes
 				</RouterLink>
@@ -25,3 +36,20 @@
 		</div>
 	</nav>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const showMobileNav = ref(true)
+
+</script>
+
+<style scoped>
+@media (max-width: 1023px) {
+	.navbar-menu {
+		position: absolute;
+		left: 0;
+		width: 100%;
+	}
+}
+</style>
