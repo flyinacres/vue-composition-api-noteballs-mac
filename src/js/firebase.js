cb20d9d079c0c969d5b6bb725a6ebe15378c611f
flyinacres <ron-github@rfgf.com>
2023-02-02 12:57:56 -0800

    Basic Firebase setup

---
+++ src/js/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgKIAxSz26lHRSwfiOIS6Z4fQ3w9lhOf0",
+  authDomain: "noteballs2-80d3e.firebaseapp.com",
+  projectId: "noteballs2-80d3e",
+  storageBucket: "noteballs2-80d3e.appspot.com",
+  messagingSenderId: "628986228052",
+  appId: "1:628986228052:web:8a4fc232aa41d75a69ecc1"
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
