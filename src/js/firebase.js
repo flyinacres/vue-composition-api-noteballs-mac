4ff9b91fc97695bb46aab16d0cc10dc4137cd3de
flyinacres <ron-github@rfgf.com>
2023-02-02 13:13:58 -0800

    reading from firestore db

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
-
+import { getFirestore } from "firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,3 +14,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+	db
+}
--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,19 +1,32 @@
 
 import { defineStore } from 'pinia'
+import { collection, getDocs } from "firebase/firestore"
+import {db} from '@/js/firebase'
 
 export const useStoreNotes = defineStore('storeNotes', {
 	state: () => {
 		return { 
-			notes: [{
-				id: 'id1',
-				content: "Amet voluptate laboris dolore cillum excepteur et nulla in. Minim mollit non adipisicing aliquip quis elit eu dolor eu ad deserunt ea ad. Lorem occaecat irure commodo dolor quis tempor sunt enim enim. Enim est commodo do pariatur non laborum incididunt deserunt et duis. Amet occaecat laborum aliquip eu ullamco ipsum dolor esse deserunt sint officia. Et nisi deserunt et do Lorem."	
-			}, {
-				id: 'id2',
-				content: "Bobbie eiusmod velit mollit officia quis ea qui. Aute duis labore labore laborum. Veniam incididunt laboris sint excepteur culpa non adipisicing minim reprehenderit ullamco excepteur proident ex nisi. Commodo enim est cupidatat adipisicing esse occaecat occaecat laboris pariatur occaecat sint. Pariatur id consequat minim cillum proident nostrud adipisicing fugiat veniam."	
-			}
+			notes: [
+			// 	{
+			// 	id: 'id1',
+			// 	content: "Amet voluptate laboris dolore cillum excepteur et nulla in. Minim mollit non adipisicing aliquip quis elit eu dolor eu ad deserunt ea ad. Lorem occaecat irure commodo dolor quis tempor sunt enim enim. Enim est commodo do pariatur non laborum incididunt deserunt et duis. Amet occaecat laborum aliquip eu ullamco ipsum dolor esse deserunt sint officia. Et nisi deserunt et do Lorem."	
+			// }, {
+			// 	id: 'id2',
+			// 	content: "Bobbie eiusmod velit mollit officia quis ea qui. Aute duis labore labore laborum. Veniam incididunt laboris sint excepteur culpa non adipisicing minim reprehenderit ullamco excepteur proident ex nisi. Commodo enim est cupidatat adipisicing esse occaecat occaecat laboris pariatur occaecat sint. Pariatur id consequat minim cillum proident nostrud adipisicing fugiat veniam."	
+			// }
 		]}
 	},
 	actions: {
+		async getNotes() {
+			const querySnapshot = await getDocs(collection(db, "notes"))
+			querySnapshot.forEach((doc) => {
+  				let note = { 
+					id: doc.id,
+					content: doc.data().content
+				}
+  				this.notes.push(note)
+				})
+		},
 		addNote(newNoteContent) {
 			let currentDate = new Date().getTime()
 			let id = currentDate.toString()
