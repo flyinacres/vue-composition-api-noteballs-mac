ca0c6eb0968c436d9238766a9cd4d3ee754c947d
flyinacres <ron-github@rfgf.com>
2023-02-03 13:01:31 -0800

    Display the creation data

--- src/components/Notes/Note.vue
+++ src/components/Notes/Note.vue
@@ -3,8 +3,9 @@
 	<div class="card mb-4">
 		<div class="card-content">
 			{{ note.content }}
-			<div class="has-text-right has-text-grey-light mt-2">
-				<small>{{ characterLength }}</small>
+			<div class="columns is-mobile has-text-grey-light mt-2">
+				<small class="column">{{dateFormatted}}</small>
+				<small class="column has-text-right">{{ characterLength }}</small>
 			</div>
 		</div>
 		<footer class="card-footer">
@@ -32,7 +33,7 @@
 <script setup>
 import { computed, reactive } from 'vue'
 import ModalDeleteNote from '@/components/Notes/ModalDeleteNote.vue'
-
+import { useDateFormat } from '@vueuse/core'
 
 
 const props = defineProps({
@@ -49,6 +50,11 @@ const characterLength = computed(() => {
 		return len + " characters"
 	}
 )
+
+const dateFormatted = computed(() => {
+	let date = new Date(parseInt(props.note.date))
+	return useDateFormat(date, 'MM-DD-YYYY@HH:mm').value
+})
 
 const modals = reactive({
 	deleteNote: false
