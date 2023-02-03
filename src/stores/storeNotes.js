18c83ac55015df75729ae135c796be2c311fb954
flyinacres <ron-github@rfgf.com>
2023-02-02 23:36:26 -0800

    order by date and cleanup

--- src/stores/storeNotes.js
+++ src/stores/storeNotes.js
@@ -1,28 +1,22 @@
 
 import { defineStore } from 'pinia'
-import { collection, onSnapshot, setDoc, doc, deleteDoc, updateDoc } from "firebase/firestore"
+import { collection, onSnapshot, setDoc, doc, deleteDoc, updateDoc, query, orderBy } from "firebase/firestore"
 import {db} from '@/js/firebase'
 
 const notesCollectionRef = collection(db, "notes")
+const notesCollectionQuery = query(notesCollectionRef, orderBy("id", 'desc'));
 
 export const useStoreNotes = defineStore('storeNotes', {
 	state: () => {
 		return { 
 			notes: [
-			// 	{
-			// 	id: 'id1',
-			// 	content: "Amet voluptate laboris dolore cillum excepteur et nulla in. Minim mollit non adipisicing aliquip quis elit eu dolor eu ad deserunt ea ad. Lorem occaecat irure commodo dolor quis tempor sunt enim enim. Enim est commodo do pariatur non laborum incididunt deserunt et duis. Amet occaecat laborum aliquip eu ullamco ipsum dolor esse deserunt sint officia. Et nisi deserunt et do Lorem."	
-			// }, {
-			// 	id: 'id2',
-			// 	content: "Bobbie eiusmod velit mollit officia quis ea qui. Aute duis labore labore laborum. Veniam incididunt laboris sint excepteur culpa non adipisicing minim reprehenderit ullamco excepteur proident ex nisi. Commodo enim est cupidatat adipisicing esse occaecat occaecat laboris pariatur occaecat sint. Pariatur id consequat minim cillum proident nostrud adipisicing fugiat veniam."	
-			// }
 		]}
 	},
 	actions: {
 		async getNotes() {
 
 			// This code dynamically updates data as it is changed on the server
-			const unsubscribe = onSnapshot(notesCollectionRef, (querySnapshot) => {
+			const unsubscribe = onSnapshot(notesCollectionQuery, (querySnapshot) => {
 				let notes = []
 				querySnapshot.forEach((doc) => {
 					let note = { 
@@ -37,28 +31,16 @@ export const useStoreNotes = defineStore('storeNotes', {
 		async addNote(newNoteContent) {
 			let currentDate = new Date().getTime()
 			let id = currentDate.toString()
-		
-			// let note = {
-			// 	id,
-			// 	content: newNoteContent
-			// }
-			//this.notes.unshift(note)
 
-			// Add a new document in collection "cities"
 			await setDoc(doc(notesCollectionRef, id), {
-			content: newNoteContent
+			content: newNoteContent,
+			id
 			})
 		},
 		async deleteNote(idToDelete) {
-			//this.notes = this.notes.filter(note => {return note.id != idToDelete} )
 			await deleteDoc(doc(notesCollectionRef, idToDelete))
 		},
 		async updateNote(id, content) {
-			// let index = this.notes.findIndex(note => {
-			// 	return note.id === id
-			// })
-			// this.notes[index].content = content
-
 			await updateDoc(doc (notesCollectionRef, id), {
 				content: content
 			})
