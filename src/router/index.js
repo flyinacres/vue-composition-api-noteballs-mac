6c4f431e0d3a5849ca3cab61f05d48686745867f
flyinacres <ron-github@rfgf.com>
2023-02-05 13:24:31 -0800

    Finished route protection

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import ViewNotes from '@/views/ViewNotes.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import ViewEditNote from '@/views/ViewEditNote.vue'
 import ViewAuth from '@/views/ViewAuth.vue'
+import { useStoreAuth } from '@/stores/StoreAuth'
+
 
 const routes = [
 	{
@@ -35,6 +37,16 @@ const router = createRouter({
 
 // A hook which is fired everytime a user tries to go to a new route
 router.beforeEach( async (to, from) => {
+	const storeAuth = useStoreAuth()
+
+	if (!storeAuth.user.id && to.name !== 'auth')  {
+		return { name: 'auth'}
+	}
+
+	// return false just stops them from changing pages
+	if (storeAuth.user.id && to.name === 'auth') {
+		return false
+	}
 	console.log('to: ', to)
 })
 
